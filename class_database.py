26520fa985fdf1fee7fff4c972a1065ca2909448
Kirill Shmidt <83780823+KirillSchmidt@users.noreply.github.com>
2023-12-10 22:07:51 +0200

    second day progress (not much)
    Unfortunately, shit is not really working that well, but I have a great start in my junk_test.py, which I will be improving. I hope to get a properly fetched CPU dataset by the next session

--- class_database.py
+++ class_database.py
@@ -1,11 +1,5 @@
-import requests
 import json
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.select import Select
-from time import sleep
-
-driver = webdriver.Chrome()
+from time import sleep, time
 
 
 class Database:
@@ -16,9 +10,15 @@ class Database:
         self._all_gpus_link = gpus_link
         self._cpu_list = []
         self._gpu_list = []
+        self._cpu_list_location = "all_jsons/cpu_list.json"
+        self._gpu_list_location = "all_jsons/gpu_list.json"
 
-    def update_cpu_list(self):
+    def update_cpu_list(self, method: str):
         def with_selenium():
+            from selenium import webdriver
+            from selenium.webdriver.common.by import By
+            from selenium.webdriver.support.select import Select
+            driver = webdriver.Chrome()
             driver.get(self._all_cpus_link)
             sleep(3)
             select = Select(driver.find_element(by=By.NAME, value="cputable_length"))
@@ -30,19 +30,62 @@ class Database:
             headers = ['CPU name', 'Cores', 'CPU Mark', 'Thread Mark', 'TDP(W)', 'Socket', 'Category']
 
             tbody = table.find_element(by=By.CSS_SELECTOR, value='tbody')
-            for row in tbody.find_elements(by=By.CSS_SELECTOR, value='tr'):
-                _list = row.text.split()
-                temp = {}
-                for i in range(len(headers) - 1):
-                    temp.update({headers[-1 - i]: _list.pop(-1)})
 
-                temp.update({headers[0]: " ".join(_list)})
+            start_time = time()
+            trs = tbody.find_elements(by=By.CSS_SELECTOR, value='tr')
+            end_time = time()
+            print(f'Found all table rows in {end_time - start_time}')
+
+            start_time = time()
+            for row in trs:
+                text = row.text
+                temp = {"HTML text": text}
+
+                at_index = text.find('@')
+                if at_index != -1:
+                    temp.update({headers[0]: text[0:at_index].rstrip()})  # updates the name
+                    text = text[at_index+1:].lstrip()  # removes text up to @ symbol
+                    # proceeds to add properties in orders of headers
+                    # can cause errors if more than one category is present and written separated by comma (,)
+                    _list = text.split()
+
+                    freq = _list.pop(0).replace("+", "")  # this could be either "2.20", "2.20+" or "2.20GHz"
+                    try:
+                        freq = float(freq)
+                        freq = str(freq) + _list.pop(0)
+                    except ValueError as vr:
+                        if "could not convert string to float:" in vr.__str__():
+                            temp.update({"Frequency": freq})
+                        else:
+                            print(f"Found a ValueError which is not due to float conversion:\n{vr}")
+
+                    for header in headers[1:]:
+                        temp.update({header: _list.pop(0)})
+                else:
+                    # here we start from the end and go to the beginning, because we don't know how long the name is
+                    _list = text.split()
+                    temp.update({"original_list": _list.copy()})
+                    # There could be 1 or 2 categories. In the latter case, they will be separated by ", " which is meh
+                    if "," in _list[-2]:
+                        temp.update({headers[-1]: " ".join(_list[-2:])})
+                        _list = _list[0:-2]
+                    else:
+                        temp.update({headers[-1]: _list.pop(-1)})
+                    for i in range(len(headers) - 2):
+                        temp.update({headers[-2 - i]: _list.pop(-1)})
+                    temp.update({headers[0]: " ".join(_list)})
                 self._cpu_list.append(temp)
 
-            with open("cpu_list.json", 'w') as file:
-                json.dump({"data": self._cpu_list}, file, indent=4)
+            end_time = time()
+            print(f'Finished fetching in {end_time - start_time}')
+
+            with open(self._cpu_list_location, 'w') as file:
+                json.dump({"cpu_list": self._cpu_list}, file, indent=4)
+            self._cpu_list = []
+            return {}
 
         def with_requests():
+            import requests
             cookies = "PHPSESSID=1epjoi3d5vasbfjtdk2j15qctu; _gid=GA1.2.1601590461.1702139575; _ga=GA1.1.1336255102.1702051043; _ga_CMWM67JT90=GS1.1.1702139575.2.1.1702141329.0.0.0"
             response = requests.get("https://www.cpubenchmark.net/data/?_=1702141329768", headers={"Cookie": cookies})
             if response.status_code == 200:
@@ -50,12 +93,42 @@ class Database:
                 del response
                 with open('cpu_response_example.json', 'w') as example:
                     json.dump(data, example, indent=4)
+            return {"error": "This feature is not available yet"}
 
-        with_selenium()
-        return "stopped"
+        if method in ['s', 'sel', 'selenium']:
+            response = with_selenium()
+        elif method in ['r', 'req', 'request', 'requests']:
+            response = with_requests()
+        else:
+            return {'error': "wrong method"}
+
+        if 'error' not in response.keys():
+            return {"status": 200, "message": "updated successfully"}
+        else:
+            return response
+
+    def get_cpu_list(self):
+        try:
+            with open(self._cpu_list_location, 'x'):
+                print("CPU List wasn't found in the database. Fetching the values from the website")
+                self.update_cpu_list(method='selenium')
+                return self.get_cpu_list()
+        except FileExistsError:
+            print("Found a record in the database. Returning...")
+            with open(self._cpu_list_location, 'r') as data_file:
+                return json.load(data_file)['cpu_list']
 
 
 db = Database()
 
 if __name__ == '__main__':
-    print(db.update_cpu_list())
+    print(db.update_cpu_list(method='selenium'))
+    cpu_list = db.get_cpu_list()
+    temp = []
+    for cpu in cpu_list:
+        try:
+            if int(cpu['Cores']) >= 4:
+                temp.append(cpu)
+        except ValueError:
+            print(f"Non-integer value of cores found:\n{cpu}")
+    print(len(temp))
--- junk_test.py
+++ junk_test.py
@@ -1,9 +1,29 @@
-row = "AArch64 rev 2 (aarch64) 8 2,320 898 NA Unknown Unknown"
-headers = ['CPU name', 'Cores', 'CPU Mark', 'Thread Mark', 'TDP(W)', 'Socket', 'Category']
-_list = row.split()
-temp = {}
-for i in range(len(headers) - 1):
-    temp.update({headers[-1 - i]: _list.pop(-1)})
+import json
 
-temp.update({headers[0]: " ".join(_list)})
-print(temp)
+with open("all_jsons/cpu_list.json", 'r') as file:
+    cpu_list = json.load(file)['cpu_list']
+
+html_text = [i['HTML text'] for i in cpu_list]
+del cpu_list
+# print(len(html_text))
+
+result = []
+headers = ['CPU name', 'Cores', 'CPU Mark', 'Thread Mark', 'TDP(W)', 'Socket', 'Category']
+categories = ["Unknown", "Desktop", "Laptop", "Server", "Mobile/Embedded"]
+for text in html_text:
+    _temp = {"HTML text": text}
+    _list = text.split()
+    # extracting categories
+    if "," in _list[-2] and _list[-2].replace(",", "") in categories:
+        _temp.update({headers[-1]: " ".join(_list[-2:])})
+        _list = _list[0:-2]
+    elif _list[-1] in categories:
+        _temp.update({headers[-1]: _list.pop(-1)})
+    else:
+        print(f"Unknown category in a list:\n{_list}")
+    # up to here, the categories are extracted fully correctly
+    # extracting socket
+
+    result.append(_temp)
+
+print(len(result))
