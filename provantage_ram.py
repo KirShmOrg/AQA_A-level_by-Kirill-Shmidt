9bb7e0511ae94e469cf024e4ebdbba62716493a1
school computer <school.email@gmail.com>
2024-04-02 09:17:16 +0300

    provantage_ram.py changes: * removed unnecessary junk * transfered another js function from the website * introduced other methods (TLDR)

--- provantage_ram.py
+++ provantage_ram.py
@@ -24,52 +24,64 @@ class RAMManufacturer:
     def js_name(self) -> str:
         return self.__js_name
 
+    @staticmethod
+    def fetch_all() -> list:
+        from selenium.webdriver import Chrome
+        from selenium.webdriver.common.by import By
+        all_manufacturers = []
+        driver = Chrome()
+        driver.get(BASE_URL)
+        manufs_div = driver.find_element(by=By.ID, value="AT2001")
+        for manuf_a in manufs_div.find_elements(by=By.CSS_SELECTOR, value='a'):
+            href = manuf_a.get_property('href')
+            js_name = href[href.find('(') + 2:href.find(',') - 1]
+            manufacturer_name = manuf_a.find_element(by=By.CLASS_NAME, value="choice").text
+            manufacturer_name = manufacturer_name[0: manufacturer_name.index("(") - 1]
+            manufacturer = RAMManufacturer(human_name=manufacturer_name, js_name=js_name)
+            all_manufacturers.append(manufacturer)
+
+        return all_manufacturers
+
     def __repr__(self) -> str:
         return f"RAMManufacturer <{self.__human_name}> access by <{self.js_name}>"
 
+class Filters:
+    def __init__(self, with_update: bool = False):
+        if with_update:
+            raise NotImplementedError("Nah-uh, wait")
+        # implementation for now
+        # TODO: redo this from a local file
 
-def get_generation():
-    page = requests.get(BASE_URL)
-    soup = BeautifulSoup(page.text, features="html.parser")
-    # searchbox = soup.find('input', attrs={'class': "SEARCHBOX"})
+    def
 
-    # ID of div with all manufacturers: "AT2001", class="sel". tag in request header: MAN={value}
-    # where value is the parameter of js function inside this div from div.a.href
-    manufacturers_div = soup.find('div', attrs={'id': "AT2001"})
-    for element in soup.find_all('a', attrs={'class': 'choice'}):
-        print(element.text)
+def generate_link(parameters: dict[str, str]):
+    link = BASE_URL
+    initial_parameters = {"category": "RAM modules"}
 
 
-def get_filters():
-    from selenium.webdriver import Chrome
-    driver = Chrome()
-    driver.get(BASE_URL)
-    time.sleep(3)
-    from selenium.webdriver.common.by import By
-    # all_tables = driver.find_element(by=By.ID, value='ATTRIB1').find_elements(by=By.XPATH,
-    #                                                                           value='//table[@class="sel"]')
+def human_param_to_provantage_code(human_param: str) -> str:
+    checkup_table = {
+        "Product Type": "31100",
+        "Memory Size": "3113035",
+        "Memory Technology": "31113713",
+        "Memory Speed": "311971",
+        "Form Factor": "311506"
+    }
+    if human_param in checkup_table.keys():
+        return checkup_table[human_param]
 
-    manufacturers_div = driver.find_element(by=By.ID, value="AT2001")
-    for a_tag in manufacturers_div.find_elements(by=By.TAG_NAME, value='a'):
-        href = a_tag.get_property('href')
-        js_name = href[href.find('(') + 2:href.find(',') - 1]
-        manufacturer_name = a_tag.find_element(by=By.CLASS_NAME, value="choice").text
-        manufacturer_name = manufacturer_name[0: manufacturer_name.index("(") - 1]
-        manufacturer = RAMManufacturer(human_name=manufacturer_name, js_name=js_name)
-        print(manufacturer)
 
-        next_link = BASE_URL + aLinkMCCopy(manufacturer.js_name, '2001')
-        print(next_link)
-    time.sleep(120)
 
 def print_notes():
     human_to_provantage_ram_filter: dict[str, str] = {
         "size": "3113035",
         "ddr_type": "31113713",
-        "speed": "311971"
+        "speed": "311971",
+        "form_factor": "311506",
+        "": ""
     }
 
-#
+
 def aLinkMCCopy(t: str, b: str):
     # function ALinkMC(t,b){var a='/service/searchsvcs/B-CRAMM?';if(b==2001)a+='MAN='+t;else a+='CAT='+t;window.location.href=a;}
     a = '/service/searchsvcs/B-CRAMM?'
@@ -87,5 +99,5 @@ def aLinkCopy():  # Used heavily, from what it seems
 
 
 if __name__ == '__main__':
-    # get_generation()
-    get_filters()
+    print_notes()
+    print(RAMManufacturer.fetch_all())
