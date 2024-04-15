2192b78f2711d1b489819cb4dc9f0558bf2d93c5
Kirill Shmidt <83780823+KirillSchmidt@users.noreply.github.com>
2024-04-15 19:41:08 +0300

    Finally solved the issue with navbar
    Turns out you have to only extend the blocks in your "base.html" if you inherit it from "bootstrap/base.html", not write your own blocks

--- flask_webpage/experiment.py
+++ flask_webpage/experiment.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap, BOOTSTRAP_VERSION
 
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 print(f'{BOOTSTRAP_VERSION = }')
 
+
 @app.route('/<name>')
 def index(name):
     return render_template('index.html', name=name)
+
 
 @app.route('/base')
 def base():
